1791ecbd8ed8b4df8c6fd5bf03238e1338797a1c
AbrRahman <abrahman.est@gmail.com>
2022-11-09 14:43:24 +0600

    added header

--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -1,9 +1,28 @@
 import React from 'react';
-
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <div>
-            <h1>This is Header </h1>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">Stock Market</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link as={Link} to='/'>Home</Nav.Link>
+                            <Nav.Link as={Link} to='/service'>Service</Nav.Link>
+                            <Nav.Link as={Link} to='/blog'>Blog</Nav.Link>
+                            <Nav.Link as={Link} to='/login'><Button>Login</Button></Nav.Link>
+                            <Nav.Link as={Link} to='/register'><Button>Register</Button></Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
         </div>
     );
 };

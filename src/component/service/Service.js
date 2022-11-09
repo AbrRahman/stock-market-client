4b88315a6ae4ecf047c4a034e3c25d4a6227475c
AbrRahman <abrahman.est@gmail.com>
2022-11-10 01:59:01 +0600

    service page photot effect

--- src/component/service/Service.js
+++ src/component/service/Service.js
@@ -7,7 +7,7 @@ const Service = () => {
     const services = useLoaderData();
     return (
         <Container className='my-4'>
-            <h3 className='text-center py-4'>Our Product</h3>
+            <h3 className='text-center py-4'>Our Service</h3>
             <Row xs={1} sm={1} md={2} lg={3} className="g-4">
                 {
                     services.map(service => <ServiceCard key={service._id} service={service}></ServiceCard>)
--- src/component/serviceCard/ServiceCard.js
+++ src/component/serviceCard/ServiceCard.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { Button, Card, Col } from 'react-bootstrap';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import 'react-photo-view/dist/react-photo-view.css';
 import { Link } from 'react-router-dom';
-
 const ServiceCard = ({ service }) => {
     const { name, image, price, details, _id } = service;
     return (
         <Col>
             <Card>
-                <Card.Img variant="top" className='w-100' style={{ maxHeight: "262px" }} src={image} />
+                <PhotoProvider>
+                    <PhotoView src={image}>
+                        <Card.Img variant="top" className='w-100' style={{ maxHeight: "262px", cursor: "pointer" }} src={image} />
+                    </PhotoView>
+                </PhotoProvider>
                 <Card.Body>
                     <Card.Title>{name}</Card.Title>
                     <Card.Text>
                         {details.length > 100 ? details.slice(0, 100) + "..." : details}
                     </Card.Text>
-                    <h4 className='text-warning '>{price} $</h4>
+                    <h4 className='text-warning '>Price : {price}$</h4>
                 </Card.Body>
                 <Card.Footer className=''>
                     <Link to={`/service/:${_id}`}> <Button className='btn-sm'>Details</Button></Link>
